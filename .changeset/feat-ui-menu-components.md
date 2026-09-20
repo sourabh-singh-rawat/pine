82c08f14a675d8de7ed1e69c48c6128df0baa658
sourabh-singh-rawat <sourabh.rawatcc@gmail.com>
2026-09-20 17:20:54 +0530

    feat(ui): add Menu and MenuItem components

---
+++ .changeset/feat-ui-menu-components.md
@@ -0,0 +1,6 @@
+---
+"@pine/ui": minor
+"@pine/pine-web": patch
+---
+
+feat(ui): add Menu and MenuItem components
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,6 +2,7 @@ export * from "./appBars";
 export * from "./buttons";
 export * from "./forms";
 export * from "./loading";
+export * from "./menus";
 export * from "./modal";
 export * from "./progress";
 export * from "./table";
